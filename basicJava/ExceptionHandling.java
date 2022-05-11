f152391f3c0e35340852b5196152ea8b711d0b05
vasanth2030 <vasanths2030@gmail.com>
2022-05-11 21:24:09 +0530

    day 5

--- basicJava/ExceptionHandling.java
+++ basicJava/ExceptionHandling.java
@@ -4,22 +4,38 @@ import java.util.Scanner;
 
 public class ExceptionHandling {
 	public static void main(String[] args) {
-		Scanner sc=new Scanner(System.in);
+		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter two numbers: ");
-		int num1=sc.nextInt();
-		int num2=sc.nextInt();
+		int num1 = sc.nextInt();
+		int num2 = sc.nextInt();
 		
-		divide(num1,num2);
-		
+		divide(num1, num2);
+
 		System.out.println("End of code.");
-		
+
 	}
 
 	public static void divide(int num1, int num2) {
-		
-		int div= num1/num2;
-		System.out.println("Division value: "+div);
-		
+
+		try {
+			int div = num1 / num2;
+			System.out.println("Division value: " + div);
+//			throw new RuntimeException("Manually thrown Exception");
+		} catch (ArithmeticException e) {
+			System.out.println(e.getMessage());
+			System.out.println("Enter a non-zero divisor");
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+			System.out.println("Caught exception throwed by the 'throw' keyword");
+		}finally {
+			System.out.println("Inside finally");
+		}
+	}
+	
+	public static void div(int a,int b) throws ArithmeticException
+	{
+		int div=a/b;
+		System.out.println(div);
 	}
 
 }
