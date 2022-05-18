f17a76697ce5db3ba7c9d838475aacdc21559e59
vasanth2030 <vasanths2030@gmail.com>
2022-05-18 21:22:37 +0530

    day 10

--- linkedlist/LinkedlistMain.java
+++ linkedlist/LinkedlistMain.java
@@ -2,6 +2,25 @@ package linkedlist;
 
 public class LinkedlistMain {
 	public static void main(String[] args) {
+		Node head=new Node(10);
+		Node.insertNodeAtEnd(head, 20);
+		Node.insertNodeAtEnd(head, 30);
+		Node.insertNodeAtEnd(head, 40);
+		Node.insertNodeAtEnd(head, 50);
+		Node.insertNodeAtEnd(head, 60);
 		
+		Node.printLinkedList(head);
+		System.out.println();
+		
+		head=Node.deletePos(head, 1);
+		Node.printLinkedList(head);
+		System.out.println();
+		
+		head=Node.deleteValue(head, 20);
+		Node.printLinkedList(head);
+		System.out.println();
+		
+		head= Node.insertAtPos(head, 70, 3);
+		Node.printLinkedList(head);
 	}
 }	
--- linkedlist/Node.java
+++ linkedlist/Node.java
@@ -9,5 +9,77 @@ public class Node {
 		this.data=data;
 		next=null;
 	}
+	
+	public static void insertNodeAtEnd( Node head, int data)
+	{
+		Node newNode=new Node(data);
+		Node curr=head;
+		while(curr.next!=null)
+		{
+			curr=curr.next;
+		}
+		
+		curr.next=newNode;
+		
+	}
+	
+	public static Node insertAtPos(Node head, int data, int pos) {
+		Node newNode = new Node(data);
+		if(pos==1)
+		{
+			newNode.next=head;
+			return newNode;
+		}
+
+		Node curr=head;
+		for(int i=1;i<pos-1;i++)
+		{
+			curr=curr.next;
+		}
+		newNode.next=curr.next;
+		curr.next=newNode;
+		return head;
+	}
+	
+	public static Node deletePos(Node head, int pos)
+	{
+		if(pos==1)
+		{
+			return head.next;
+		}
+		Node curr=head;
+		for(int i=1;i<pos-1;i++)
+		{
+			curr=curr.next;
+		}
+		
+		curr.next=curr.next.next;
+		return head;
+	}
+	
+	public static Node deleteValue(Node head,int val)
+	{
+		if(head.data==val)
+			return head.next;
+		
+		Node curr=head;
+		while(curr.next.data!=val && curr.next!=null)
+		{
+			curr=curr.next;
+		}
+		curr.next=curr.next.next;
+		return head;
+		
+	}
+	
+	public static void printLinkedList(Node head)
+	{
+		Node curr=head;
+		while(curr!=null)
+		{
+			System.out.print(curr.data+" --> ");
+			curr=curr.next;
+		}
+	}
 
 }
